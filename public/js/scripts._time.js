63a2cc5d4b567166ee46f795f821953711a857af
Diana Majolli Andre <dianamajolli@hotmail.com>
2023-11-26 17:28:08 -0500

    Update API time

--- public/js/scripts._time.js
+++ public/js/scripts._time.js
@@ -1,11 +1,22 @@
-const API_KEY = 'a532733febc143feb306567cce782947';
-    const URL = `https://timezone.abstractapi.com/v1/current_time/?api_key=${API_KEY}&location=Montreal`;
-    fetch(URL)
-      .then(response => response.json())
-      .then(data => {
-        const time = new Date(data.datetime);
-        const options = { hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-        const formattedTime = time.toLocaleString('en-US', options);
-        document.getElementById('time').textContent = formattedTime;
-      })
-      .catch(error => console.error(error));
+const API_KEY = "a532733febc143feb306567cce782947";
+const URL = `https://timezone.abstractapi.com/v1/current_time/?api_key=${API_KEY}&location=Montreal`;
+
+function time_api() {
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      const time = new Date(data.datetime);
+      const options = {
+        hour: "numeric",
+        minute: "numeric",
+        //second: "numeric",
+        timeZoneName: "short",
+      };
+      const formattedTime = time.toLocaleString("en-US", options);
+      document.getElementById("time").textContent = formattedTime;
+      setTimeout(time_api, 2000);
+    })
+    .catch((error) => console.error(error));
+}
+
+time_api();
